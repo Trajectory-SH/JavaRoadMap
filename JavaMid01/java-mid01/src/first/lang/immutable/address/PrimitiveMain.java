ad9e02d1163cd17b70592285fd760a2e7c35e447
Trajectory-SH <trajectory0611@gmail.com>
2025-05-02 09:25:04 +0900

    20250502_mid01_section2

---
+++ JavaMid01/java-mid01/src/first/lang/immutable/address/PrimitiveMain.java
@@ -0,0 +1,11 @@
+package first.lang.immutable.address;
+
+public class PrimitiveMain {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a;
+        System.out.println("a= " + a);
+        System.out.println("b= " + b);
+
+    }
+}
--- JavaMid01/java-mid01/src/first/lang/object/equals/UserV2.java
+++ JavaMid01/java-mid01/src/first/lang/object/equals/UserV2.java
@@ -1,9 +1,20 @@
 package first.lang.object.equals;
 
+import java.util.Objects;
+
 public class UserV2 {
     private String id;
 
     public UserV2(String id) {
         this.id = id;
     }
+
+
+    @Override
+    public boolean equals(Object object) {
+        UserV2 user = (UserV2) object;
+        return id.equals(user.id);
+    }
+
+
 }
