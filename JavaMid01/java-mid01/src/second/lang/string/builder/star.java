ad48b977a3ee6985329bc45009aa5edd3008c6bc
Trajectory-SH <trajectory0611@gmail.com>
2025-05-19 11:35:59 +0900

    "2025-05-19"

---
+++ JavaMid01/java-mid01/src/second/lang/string/builder/star.java
@@ -0,0 +1,12 @@
+package second.lang.string.builder;
+
+public class star {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int m = 0;m<i ; m++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
