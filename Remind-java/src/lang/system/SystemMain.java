a5ea15926e56389307e8c1d5524f9ca409a3683c
Trajectory-SH <trajectory0611@gmail.com>
2025-05-20 16:00:48 +0900

    "2025-05-20" Java-mid01 Section.05 System Remind-04

---
+++ Remind-java/src/lang/system/SystemMain.java
@@ -0,0 +1,11 @@
+package lang.system;
+
+import java.util.Random;
+
+public class SystemMain {
+    public static void main(String[] args) {
+        System.out.println("getenv :"+ System.getenv());
+        System.out.println("시스템 속성"+ System.getProperties());
+
+    }
+}
