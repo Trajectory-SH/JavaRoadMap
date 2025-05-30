6ab71dd77c5f760afedfff16fdfac66b0b7aaa56
Trajectory-SH <trajectory0611@gmail.com>
2025-05-30 16:06:32 +0900

    JAVA-MID02 List Collection 20250530_02

---
+++ JavaMid02/java-mid02/src/collection/list/MyListPerformanceTest.java
@@ -0,0 +1,53 @@
+package collection.list;
+
+public class MyListPerformanceTest {
+    public static void main(String[] args) {
+        int size = 50_000;
+        System.out.println("==MyArrayList==");
+        addFirst(new MyArrayList<>(), size);
+        addMid(new MyArrayList<>(), size);
+        MyArrayList<Integer> arrayList = new MyArrayList<>();
+        addLast(arrayList, size);
+
+        System.out.println("==MyLinkedList==");
+        addFirst(new MyLinkedList<>(), size);
+        addMid(new MyLinkedList<>(), size);
+        MyLinkedList<Integer> linkedList = new MyLinkedList<>();
+        addLast(linkedList, size);
+
+
+
+
+
+
+
+
+    }
+
+    private static void addFirst(MyList<Integer> list, int size) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            list.add(0, i);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("list 앞에 추가 => 크기: " + size + ", 실행 시간: " + (endTime - startTime) + "ms");
+    }
+
+    private static void addMid(MyList<Integer> list, int size) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            list.add(i/2, i);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("list 중간에 추가 => 크기: " + size + ", 실행 시간: " + (endTime - startTime) + "ms");
+    }
+
+    private static void addLast(MyList<Integer> list, int size) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            list.add(i);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("list 마지막에 추가 => 크기: " + size + ", 실행 시간: " + (endTime - startTime) + "ms");
+    }
+}
